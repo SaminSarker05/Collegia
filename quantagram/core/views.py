4547f17bed16918a8ffada6466fdf386ce4ed271
Samin Sarker <saminsarker05@gmail.com>
2024-01-05 15:56:40 -0500

    updated database with Post ORM class and dummy objects

--- quantagram/core/views.py
+++ quantagram/core/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-
-posts = [
-  {
-    'author': 'Samin',
-    'title': 'Comback',
-    'content': 'all',
-    'date-posted': 'August',
-  },
-  {
-    'author': 'Sarker',
-    'title': 'Yes',
-    'content': 'everything',
-    'date-posted': 'june',
-  }
-]
+from .models import Post
 
 def home(request):
   context = {
-    'posts': posts,
+    'posts': Post.objects.all()
   }
   return render(request, 'core/home.html', context)
 
