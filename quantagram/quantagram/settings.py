862ddc58f87a4824950376ef3758c3e2cf0cc85b
Samin Sarker <saminsarker05@gmail.com>
2024-01-05 16:55:54 -0500

    registration form logic working

--- quantagram/quantagram/settings.py
+++ quantagram/quantagram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
