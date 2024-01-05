54d6b5454d29b5af520374260971e9752a8d300b
Samin Sarker <saminsarker05@gmail.com>
2024-01-05 01:35:43 -0500

    create base template with bootstrap integration

--- quantagram/core/views.py
+++ quantagram/core/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+posts = [
+  {
+    'author': 'Samin',
+    'title': 'Comback',
+    'content': 'all',
+    'date-posted': 'August',
+  },
+  {
+    'author': 'Sarker',
+    'title': 'Yes',
+    'content': 'everything',
+    'date-posted': 'june',
+  }
+]
+
+def home(request):
+  context = {
+    'posts': posts,
+  }
+  return render(request, 'core/home.html', context)
+
+
+def about(request):
+  return render(request, 'core/about.html')
