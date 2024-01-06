8e50739192ea726d3b0c96e51e7c2aa787d43df3
Samin Sarker <saminsarker05@gmail.com>
2024-01-06 00:57:33 -0500

    post delete, creation, detail, and update created

--- quantagram/core/urls.py
+++ quantagram/core/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about', views.about, name='about')
+  path('', PostListView.as_view(), name='home'),
+  path('about', views.about, name='about'),
+  path('post/<int:pk>/', PostDetailView.as_view(), name="detail"),
+  path('post/new/', PostCreateView.as_view(), name="create"),
+  path('post/<int:pk>/update', PostUpdateView.as_view(), name="update"),
+  path('post/<int:pk>/delete', PostDeleteView.as_view(), name="delete"),
+
+
 ]
--- quantagram/core/views.py
+++ quantagram/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 def home(request):
   context = {
@@ -8,6 +9,50 @@ def home(request):
   }
   return render(request, 'core/home.html', context)
 
-
 def about(request):
   return render(request, 'core/about.html')
+
+class PostListView(ListView):
+  model = Post
+  template_name = 'core/home.html'
+  context_object_name = 'posts'
+  ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+  model = Post
+  template_name = 'core/detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+  model = Post
+  fields = ['title', 'content']
+  template_name = 'core/create.html'
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+  model = Post
+  fields = ['title', 'content']
+  template_name = 'core/create.html'
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
+
+  def test_func(self):
+    post = self.get_object()
+    return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+  model = Post
+  template_name = 'core/delete.html'
+  success_url = '/'
+
+  def test_func(self):
+    post = self.get_object()
+    return self.request.user == post.author
+
